26c7fb2e626b42bb3b34fdb3a534e0b75dc10570
tharukeRajapaksha123 <darksparrow156@gmail.com>
2021-12-19 08:42:28 +0530

    ADD:image slider done

--- src/components/CatogaryBar/CatogaryBar.js
+++ src/components/CatogaryBar/CatogaryBar.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Container } from './CatogaryBarComponents'
+import { CategoryImage, CatogaryTitle, Container, TitleWrapper,Title, Spacer, Devider, } from './CatogaryBarComponents'
 
 const CatogaryBar = () => {
+    const i= "https://chamacomputers.lk/api/img/products/product_1356_1.png"
     return (
         <Container>
-            
+            <TitleWrapper>
+                <Title>Categories</Title>
+            </TitleWrapper>
+            <Devider/>
+            <CatogaryTitle>
+                <CategoryImage>
+                    <img src={i} alt="catefogt"/>
+                </CategoryImage>
+                <h3>Laptops</h3>
+                <Spacer/>
+            </CatogaryTitle>
+            <Devider/>
+            <CatogaryTitle>
+                <CategoryImage>
+                    <img src={i} alt="catefogt"/>
+                </CategoryImage>
+                <h3>Smart Phones</h3>
+                <Spacer/>
+            </CatogaryTitle>
+            <Devider/>
+            <CatogaryTitle>
+                <CategoryImage>
+                    <img src={i} alt="catefogt"/>
+                </CategoryImage>
+                <h3>Laptops</h3>
+                <Spacer/>
+            </CatogaryTitle>
+            <Devider/>
         </Container>
     )
 }
--- src/components/CatogaryBar/CatogaryBarComponents.js
+++ src/components/CatogaryBar/CatogaryBarComponents.js
@@ -8,12 +8,54 @@ const Container = styled.div`
     display:flex;
     flex-direction : column;
     justify-content:space-evenly;
+    padding : 20px;
 `
 
-const TitleWrapper = styled.div``
+const TitleWrapper = styled.div`
+    width : 300px;
+    margin : 15px;
+`
 
 const Title = styled.h2``
 
-const CatogaryTitle = styled.div``;
+const CatogaryTitle = styled.div`
+    display:flex;
+    height : 50px;
+    width : 100%;
+    padding : 0 15px;
+    overflow:hidden;
+    justify-content:space-between;
+    align-items:center;
+    cursor:pointer;
+    h3{
+        padding : 0 15px;
+        color:#505050;
+        letter-spacing:1.5px;
+    }
+`;
 
-export  {Container,TitleWrapper,Title,CatogaryTitle}
+const CategoryImage = styled.div`
+    width : 20%;
+    height : 100%;
+    border-radius : 10px;
+    overflow:hidden;
+    img{
+        width : 100%;
+        height: 100%;
+        object-fit:cover;
+    }
+    
+`
+
+const Spacer = styled.div`
+    flex: 1;
+`
+
+const Devider = styled.div`
+    background-color:grey;
+    width: 280px;
+    height:0.5px;
+    border-radius:10px;
+`
+
+export  {Container,TitleWrapper,Title,CatogaryTitle,CategoryImage,Spacer,Devider}
